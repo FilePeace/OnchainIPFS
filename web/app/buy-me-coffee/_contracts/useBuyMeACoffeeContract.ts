23866f898923c5fbf6c4f68dd8f66e9cffed9ee1
Daniella Mesquita <mesquitadaniella@proton.me>
2024-07-14 02:24:48 -0300

    revert previous commit

--- web/app/buy-me-coffee/_contracts/useBuyMeACoffeeContract.ts
+++ web/app/buy-me-coffee/_contracts/useBuyMeACoffeeContract.ts
@@ -9,7 +9,7 @@ export const useBuyMeACoffeeContract = generateContractHook({
   abi: BuyMeACoffeeABI,
   [baseSepolia.id]: {
     chain: baseSepolia,
-    address: '0xA03803D2cF1B78568B06531f52f7917b248E252b',
+    address: '0xcE0EBD0282e247553eb8fDdeE3281b5EC09ddD16',
   },
 
   // ... more chains for this contract go here
